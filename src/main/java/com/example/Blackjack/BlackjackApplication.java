a9461ae0c94da5ee2f4dd96428cd3a0e285a3d8b
Jakub Hukiewicz <j.hukiewicz12@gmail.com>
2020-11-11 14:57:48 +0100

    first setup

---
+++ src/main/java/com/example/Blackjack/BlackjackApplication.java
@@ -0,0 +1,13 @@
+package com.example.Blackjack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackjackApplication.class, args);
+	}
+
+}
